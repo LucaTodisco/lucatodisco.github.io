b2eea529f6fc500b519e395e60e7d9a3a5d4a978
LucaTodisco <56765692+LucaTodisco@users.noreply.github.com>
2025-04-12 22:04:58 +0200

    Update script.js

--- campionatopes/html/script.js
+++ campionatopes/html/script.js
@@ -1,4 +1,4 @@
-// Dati delle partite
+
 const partite = [
   {
     squadraCasa: "Bari",
@@ -12,7 +12,8 @@ const partite = [
     magliaCasa: "../img/kits/stagione2/bari-home.png",
     magliaOspite: "../img/kits/stagione2/parma-home.png",
     pallone: "../img/balls/stagione2/roteiro-blue.png",
-    note: "Partita giocata allo Stadio San Nicola."
+    stadioImg: "../img/stadiums/bari-estate-notte-sereno.png",
+    note: "Stadio San Nicola - Bari"
   },
   {
     squadraCasa: "Cagliari",
@@ -26,7 +27,8 @@ const partite = [
     magliaCasa: "../img/kits/stagione2/cagliari-home.png",
     magliaOspite: "../img/kits/stagione2/napoli-away.png",
     pallone: "../img/balls/stagione2/roteiro-blue.png",
-    note: "Partita giocata allo Stadio Sant'Elia."
+    stadioImg: "../img/stadiums/cagliari-estate-notte-sereno.png",
+    note: "Unipol Domus - Cagliari"
   },
   {
     squadraCasa: "Fiorentina",
@@ -34,17 +36,62 @@ const partite = [
     punteggioCasa: 1,
     punteggioOspite: 1,
     marcatoriCasa: ["Chris 67'"],
-    marcatoriOspite: ["57' Autorete"],  // Correzione da marcatoriOspiti a marcatoriOspite
+    marcatoriOspite: ["57' Autorete"],
     logoCasa: "../img/fiorentina-logo.png",
     logoOspite: "../img/reggina-logo.png",
     magliaCasa: "../img/kits/stagione2/fiorentina-home.png",
     magliaOspite: "../img/kits/stagione2/reggina-away.png",
     pallone: "../img/balls/stagione2/roteiro-blue.png",
-    note: "Partita giocata allo stadio Artemio Franchi."
+    stadioImg: "../img/stadiums/fiorentina-estate-giorno-sereno.png",
+    note: "Stadio Artemio Franchi - Firenze"
+  },
+  {
+    squadraCasa: "Genoa",
+    squadraOspite: "Juventus",
+    punteggioCasa: 0,
+    punteggioOspite: 1,
+    marcatoriCasa: [],
+    marcatoriOspite: ["76' Mattia"],
+    logoCasa: "../img/genoa-logo.png",
+    logoOspite: "../img/juventus-logo.png",
+    magliaCasa: "../img/kits/stagione2/genoa-home.png",
+    magliaOspite: "../img/kits/stagione2/juventus-third.png",
+    pallone: "../img/balls/stagione2/roteiro-blue.png",
+    stadioImg: "../img/stadiums/genoa-estate-giorno-sereno.png",
+    note: "Stadio Luigi Ferrari - Genova"
+  },
+  {
+    squadraCasa: "Inter",
+    squadraOspite: "Sampdoria",
+    punteggioCasa: 2,
+    punteggioOspite: 2,
+    marcatoriCasa: ["Mattia 59'","Simone 90'"],
+    marcatoriOspite: ["4' Antonio","51' Giampaolo"],
+    logoCasa: "../img/inter-logo.png",
+    logoOspite: "../img/sampdoria-logo.png",
+    magliaCasa: "../img/kits/stagione2/inter-home.png",
+    magliaOspite: "../img/kits/stagione2/sampdoria-away.png",
+    pallone: "../img/balls/stagione2/roteiro-blue.png",
+    stadioImg: "../img/stadiums/inter-estate-giorno-sereno.png",
+    note: "Stadio Giuseppe Meazza - Milano"
+  },
+  {
+    squadraCasa: "Lazio",
+    squadraOspite: "Roma",
+    punteggioCasa: 0,
+    punteggioOspite: 0,
+    marcatoriCasa: [],
+    marcatoriOspite: [],
+    logoCasa: "../img/lazio-logo.png",
+    logoOspite: "../img/roma-logo.png",
+    magliaCasa: "../img/kits/stagione2/lazio-home.png",
+    magliaOspite: "../img/kits/stagione2/roma-home.png",
+    pallone: "../img/balls/stagione2/roteiro-blue.png",
+    stadioImg: "../img/stadiums/lazio-estate-notte-sereno.png",
+    note: "Stadio Olimpico - Roma"
   }
 ];
 
-// Funzione per generare il calendario delle partite
 function generaPartite() {
   const calendarioDiv = document.getElementById('calendario-partite');
 
@@ -52,7 +99,6 @@ function generaPartite() {
     const partitaDiv = document.createElement('div');
     partitaDiv.classList.add('partita');
     partitaDiv.setAttribute('onclick', 'toggleDetails(this)');
-
     partitaDiv.innerHTML = `
       <div class="titolo-partita">
         <div class="logo-blocco">
@@ -65,17 +111,19 @@ function generaPartite() {
           <img src="${partita.logoOspite}" alt="${partita.squadraOspite}" class="logo-squadra">
         </div>
       </div>
-      <div class="dettagli">
-        <div class="marcatori">
-          <div class="marcatore casa">${partita.marcatoriCasa.join("<br>")}</div>
-          <div class="marcatore ospite">${partita.marcatoriOspite.join("<br>")}</div>
+      <div class="dettagli" style="background-image: url('${partita.stadioImg}');">
+        <div class="overlay">
+          <div class="marcatori">
+            <div class="marcatore casa">${partita.marcatoriCasa.join("<br>")}</div>
+            <div class="marcatore ospite">${partita.marcatoriOspite.join("<br>")}</div>
+          </div>
+          <div class="divise">
+            <img src="${partita.magliaCasa}" alt="Maglia ${partita.squadraCasa}" class="divisa">
+            <img src="${partita.pallone}" alt="Pallone" class="pallone">
+            <img src="${partita.magliaOspite}" alt="Maglia ${partita.squadraOspite}" class="divisa">
+          </div>
+          <div class="note">${partita.note}</div>
         </div>
-        <div class="divise">
-          <img src="${partita.magliaCasa}" alt="Maglia ${partita.squadraCasa}" class="divisa">
-          <img src="${partita.pallone}" alt="Pallone" class="pallone">
-          <img src="${partita.magliaOspite}" alt="Maglia ${partita.squadraOspite}" class="divisa">
-        </div>
-        <div class="note">${partita.note}</div>
       </div>
     `;
 
