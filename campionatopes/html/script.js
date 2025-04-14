2a5eb48af4db7ffc21e5b227197e8829cab53a77
LucaTodisco <56765692+LucaTodisco@users.noreply.github.com>
2025-04-14 22:37:15 +0200

    Update script.js

--- campionatopes/html/script.js
+++ campionatopes/html/script.js
@@ -1,138 +1,291 @@
-
-const partite = [
-  {
-    squadraCasa: "Bari",
-    squadraOspite: "Parma",
-    punteggioCasa: 0,
-    punteggioOspite: 1,
-    marcatoriCasa: [],
-    marcatoriOspite: ["53' David"],
-    logoCasa: "../img/bari-logo.png",
-    logoOspite: "../img/parma-logo.png",
-    magliaCasa: "../img/kits/stagione2/bari-home.png",
-    magliaOspite: "../img/kits/stagione2/parma-home.png",
-    pallone: "../img/balls/stagione2/roteiro-blue.png",
-    stadioImg: "../img/stadiums/bari-estate-notte-sereno.png",
-    note: "Stadio San Nicola - Bari"
-  },
-  {
-    squadraCasa: "Cagliari",
-    squadraOspite: "Napoli",
-    punteggioCasa: 1,
-    punteggioOspite: 1,
-    marcatoriCasa: ["Fabio 5'"],
-    marcatoriOspite: ["90' Gilberto"],
-    logoCasa: "../img/cagliari-logo.png",
-    logoOspite: "../img/napoli-logo.png",
-    magliaCasa: "../img/kits/stagione2/cagliari-home.png",
-    magliaOspite: "../img/kits/stagione2/napoli-away.png",
-    pallone: "../img/balls/stagione2/roteiro-blue.png",
-    stadioImg: "../img/stadiums/cagliari-estate-notte-sereno.png",
-    note: "Unipol Domus - Cagliari"
-  },
-  {
-    squadraCasa: "Fiorentina",
-    squadraOspite: "Reggina",
-    punteggioCasa: 1,
-    punteggioOspite: 1,
-    marcatoriCasa: ["Chris 67'"],
-    marcatoriOspite: ["57' Autorete"],
-    logoCasa: "../img/fiorentina-logo.png",
-    logoOspite: "../img/reggina-logo.png",
-    magliaCasa: "../img/kits/stagione2/fiorentina-home.png",
-    magliaOspite: "../img/kits/stagione2/reggina-away.png",
-    pallone: "../img/balls/stagione2/roteiro-blue.png",
-    stadioImg: "../img/stadiums/fiorentina-estate-giorno-sereno.png",
-    note: "Stadio Artemio Franchi - Firenze"
-  },
-  {
-    squadraCasa: "Genoa",
-    squadraOspite: "Juventus",
-    punteggioCasa: 0,
-    punteggioOspite: 1,
-    marcatoriCasa: [],
-    marcatoriOspite: ["76' Mattia"],
-    logoCasa: "../img/genoa-logo.png",
-    logoOspite: "../img/juventus-logo.png",
-    magliaCasa: "../img/kits/stagione2/genoa-home.png",
-    magliaOspite: "../img/kits/stagione2/juventus-third.png",
-    pallone: "../img/balls/stagione2/roteiro-blue.png",
-    stadioImg: "../img/stadiums/genoa-estate-giorno-sereno.png",
-    note: "Stadio Luigi Ferrari - Genova"
-  },
-  {
-    squadraCasa: "Inter",
-    squadraOspite: "Sampdoria",
-    punteggioCasa: 2,
-    punteggioOspite: 2,
-    marcatoriCasa: ["Mattia 59'","Simone 90'"],
-    marcatoriOspite: ["4' Antonio","51' Giampaolo"],
-    logoCasa: "../img/inter-logo.png",
-    logoOspite: "../img/sampdoria-logo.png",
-    magliaCasa: "../img/kits/stagione2/inter-home.png",
-    magliaOspite: "../img/kits/stagione2/sampdoria-away.png",
-    pallone: "../img/balls/stagione2/roteiro-blue.png",
-    stadioImg: "../img/stadiums/inter-estate-giorno-sereno.png",
-    note: "Stadio Giuseppe Meazza - Milano"
-  },
-  {
-    squadraCasa: "Lazio",
-    squadraOspite: "Roma",
-    punteggioCasa: 0,
-    punteggioOspite: 0,
-    marcatoriCasa: [],
-    marcatoriOspite: [],
-    logoCasa: "../img/lazio-logo.png",
-    logoOspite: "../img/roma-logo.png",
-    magliaCasa: "../img/kits/stagione2/lazio-home.png",
-    magliaOspite: "../img/kits/stagione2/roma-home.png",
-    pallone: "../img/balls/stagione2/roteiro-blue.png",
-    stadioImg: "../img/stadiums/lazio-estate-notte-sereno.png",
-    note: "Stadio Olimpico - Roma"
-  }
+const giornate = [
+	{
+	numero: 1,
+	partite: [
+	  {
+		squadraCasa: "Bari",
+		squadraOspite: "Parma",
+		punteggioCasa: 0,
+		punteggioOspite: 1,
+		marcatoriCasa: [],
+		marcatoriOspite: ["53' David"],
+		logoCasa: "../img/bari-logo.png",
+		logoOspite: "../img/parma-logo.png",
+		magliaCasa: "../img/kits/stagione2/bari-home.png",
+		magliaOspite: "../img/kits/stagione2/parma-home.png",
+		pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/bari-notte-sereno.png",
+		note: "Stadio San Nicola - Bari",
+		inProgramma: false
+	  },
+	  {
+		squadraCasa: "Cagliari",
+		squadraOspite: "Napoli",
+		punteggioCasa: 1,
+		punteggioOspite: 1,
+		marcatoriCasa: ["Fabio 5'"],
+		marcatoriOspite: ["90' Gilberto"],
+		logoCasa: "../img/cagliari-logo.png",
+		logoOspite: "../img/napoli-logo.png",
+		magliaCasa: "../img/kits/stagione2/cagliari-home.png",
+		magliaOspite: "../img/kits/stagione2/napoli-away.png",
+		pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/cagliari-notte-sereno.png",
+		note: "Unipol Domus - Cagliari",
+		inProgramma: false
+	  },
+	  {
+		squadraCasa: "Fiorentina",
+		squadraOspite: "Reggina",
+		punteggioCasa: 1,
+		punteggioOspite: 1,
+		marcatoriCasa: ["Chris 67'"],
+		marcatoriOspite: ["57' Autorete"],
+		logoCasa: "../img/fiorentina-logo.png",
+		logoOspite: "../img/reggina-logo.png",
+		magliaCasa: "../img/kits/stagione2/fiorentina-home.png",
+		magliaOspite: "../img/kits/stagione2/reggina-away.png",
+		pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/fiorentina-giorno-sereno.png",
+		note: "Stadio Artemio Franchi - Firenze",
+		inProgramma: false
+	  },
+	  {
+		squadraCasa: "Genoa",
+		squadraOspite: "Juventus",
+		punteggioCasa: 0,
+		punteggioOspite: 1,
+		marcatoriCasa: [],
+		marcatoriOspite: ["76' Mattia"],
+		logoCasa: "../img/genoa-logo.png",
+		logoOspite: "../img/juventus-logo.png",
+		magliaCasa: "../img/kits/stagione2/genoa-home.png",
+		magliaOspite: "../img/kits/stagione2/juventus-third.png",
+		pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/genoa-giorno-sereno.png",
+		note: "Stadio Luigi Ferrari - Genova",
+		inProgramma: false
+	  },
+	  {
+		squadraCasa: "Inter",
+		squadraOspite: "Sampdoria",
+		punteggioCasa: 2,
+		punteggioOspite: 2,
+		marcatoriCasa: ["Mattia 59'","Simone 90'"],
+		marcatoriOspite: ["4' Antonio","51' Giampaolo"],
+		logoCasa: "../img/inter-logo.png",
+		logoOspite: "../img/sampdoria-logo.png",
+		magliaCasa: "../img/kits/stagione2/inter-home.png",
+		magliaOspite: "../img/kits/stagione2/sampdoria-away.png",
+		pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/inter-giorno-sereno.png",
+		note: "Stadio Giuseppe Meazza - Milano",
+		inProgramma: false
+	  },
+	  {
+		squadraCasa: "Lazio",
+		squadraOspite: "Roma",
+		punteggioCasa: 0,
+		punteggioOspite: 0,
+		marcatoriCasa: [],
+		marcatoriOspite: [],
+		logoCasa: "../img/lazio-logo.png",
+		logoOspite: "../img/roma-logo.png",
+		magliaCasa: "../img/kits/stagione2/lazio-home.png",
+		magliaOspite: "../img/kits/stagione2/roma-home.png",
+		pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/lazio-notte-sereno.png",
+		note: "Stadio Olimpico - Roma",
+		inProgramma: false
+	  },
+	]
+	},
+{
+	numero: 2,
+	partite: [
+	{
+		squadraCasa: "Juventus",
+		squadraOspite: "Bari",
+		//punteggioCasa: 0,
+		//punteggioOspite: 0,
+		logoCasa: "../img/juventus-logo.png",
+		logoOspite: "../img/bari-logo.png",
+		//magliaCasa: "../img/kits/stagione2/juventus-home.png",
+		//magliaOspite: "../img/kits/stagione2/bari-home.png",
+		//pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/juventus-notte-sereno.png",
+		note: "Allianz Stadium - Torino",
+		inProgramma: true
+	},
+	{
+		squadraCasa: "Napoli",
+		squadraOspite: "Genoa",
+		//punteggioCasa: 0,
+		//punteggioOspite: 0,
+		logoCasa: "../img/napoli-logo.png",
+		logoOspite: "../img/genoa-logo.png",
+		//magliaCasa: "../img/kits/stagione2/juventus-home.png",
+		//magliaOspite: "../img/kits/stagione2/bari-home.png",
+		//pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/napoli-giorno-pioggia.png",
+		note: "Stadio Maradona - Napoli",
+		inProgramma: true
+	},
+	{
+		squadraCasa: "Parma",
+		squadraOspite: "Lazio",
+		//punteggioCasa: 0,
+		//punteggioOspite: 0,
+		logoCasa: "../img/parma-logo.png",
+		logoOspite: "../img/lazio-logo.png",
+		//magliaCasa: "../img/kits/stagione2/juventus-home.png",
+		//magliaOspite: "../img/kits/stagione2/bari-home.png",
+		//pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/parma-giorno-sereno.png",
+		note: "Stadio Ennio Tardini - Parma",
+		inProgramma: true
+	},
+	{
+		squadraCasa: "Roma",
+		squadraOspite: "Inter",
+		//punteggioCasa: 0,
+		//punteggioOspite: 0,
+		logoCasa: "../img/roma-logo.png",
+		logoOspite: "../img/inter-logo.png",
+		//magliaCasa: "../img/kits/stagione2/juventus-home.png",
+		//magliaOspite: "../img/kits/stagione2/bari-home.png",
+		//pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/roma-notte-sereno.png",
+		note: "Stadio Olimpico  - Roma",
+		inProgramma: true
+	},
+	{
+		squadraCasa: "Reggina",
+		squadraOspite: "Cagliari",
+		//punteggioCasa: 0,
+		//punteggioOspite: 0,
+		logoCasa: "../img/reggina-logo.png",
+		logoOspite: "../img/cagliari-logo.png",
+		//magliaCasa: "../img/kits/stagione2/juventus-home.png",
+		//magliaOspite: "../img/kits/stagione2/bari-home.png",
+		//pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/reggina-giorno-pioggia.png",
+		note: "Stadio Oreste Granillo - Reggio Calabria",
+		inProgramma: true
+	},
+	{
+		squadraCasa: "Sampdoria",
+		squadraOspite: "Fiorentina",
+		//punteggioCasa: 0,
+		//punteggioOspite: 0,
+		logoCasa: "../img/sampdoria-logo.png",
+		logoOspite: "../img/fiorentina-logo.png",
+		//magliaCasa: "../img/kits/stagione2/juventus-home.png",
+		//magliaOspite: "../img/kits/stagione2/bari-home.png",
+		//pallone: "../img/balls/stagione2/roteiro-blue.png",
+		stadioImg: "../img/stadiums/sampdoria-notte-sereno.png",
+		note: "Stadio Luigi Ferraris - Genova",
+		inProgramma: true
+	}
+  ]
+}
 ];
 
-function generaPartite() {
-  const calendarioDiv = document.getElementById('calendario-partite');
+function generaCalendario() {
+  const calendario = document.getElementById("calendario");
+  calendario.innerHTML = "";
 
-  partite.forEach(partita => {
-    const partitaDiv = document.createElement('div');
-    partitaDiv.classList.add('partita');
-    partitaDiv.setAttribute('onclick', 'toggleDetails(this)');
-    partitaDiv.innerHTML = `
-      <div class="titolo-partita">
-        <div class="logo-blocco">
-          <img src="${partita.logoCasa}" alt="${partita.squadraCasa}" class="logo-squadra">
-        </div>
+  giornate.forEach((giornata, index) => {
+    const giornataEl = document.createElement("div");
+    giornataEl.classList.add("giornata");
+
+    const anteprima = document.createElement("div");
+    anteprima.classList.add("anteprima-giornata");
+    anteprima.textContent = `${giornata.numero}a giornata`;
+
+    const contenuto = document.createElement("div");
+    contenuto.classList.add("contenuto-giornata");
+
+    giornata.partite.forEach((partita, i) => {
+      const partitaEl = document.createElement("div");
+      partitaEl.classList.add("partita");
+
+      const titolo = document.createElement("div");
+      titolo.classList.add("titolo-partita");
+      titolo.innerHTML = `
+        <div class="logo-blocco"><img class="logo-squadra" src="${partita.logoCasa}" alt="${partita.squadraCasa}"></div>
         <div class="nome-squadra">${partita.squadraCasa}</div>
-        <div class="punteggio">${partita.punteggioCasa} - ${partita.punteggioOspite}</div>
+        <div class="punteggio">${partita.inProgramma ? "-" : partita.punteggioCasa + " - " + partita.punteggioOspite}</div>
         <div class="nome-squadra">${partita.squadraOspite}</div>
-        <div class="logo-blocco">
-          <img src="${partita.logoOspite}" alt="${partita.squadraOspite}" class="logo-squadra">
-        </div>
-      </div>
-      <div class="dettagli" style="background-image: url('${partita.stadioImg}');">
-        <div class="overlay">
-          <div class="marcatori">
-            <div class="marcatore casa">${partita.marcatoriCasa.join("<br>")}</div>
-            <div class="marcatore ospite">${partita.marcatoriOspite.join("<br>")}</div>
-          </div>
-          <div class="divise">
-            <img src="${partita.magliaCasa}" alt="Maglia ${partita.squadraCasa}" class="divisa">
-            <img src="${partita.pallone}" alt="Pallone" class="pallone">
-            <img src="${partita.magliaOspite}" alt="Maglia ${partita.squadraOspite}" class="divisa">
-          </div>
-          <div class="note">${partita.note}</div>
-        </div>
-      </div>
-    `;
+        <div class="logo-blocco"><img class="logo-squadra" src="${partita.logoOspite}" alt="${partita.squadraOspite}"></div>
+      `;
 
-    calendarioDiv.appendChild(partitaDiv);
+      const dettagli = document.createElement("div");
+      dettagli.classList.add("dettagli");
+      dettagli.style.backgroundImage = `url('${partita.stadioImg}')`;
+
+      const overlay = document.createElement("div");
+      overlay.classList.add("overlay");
+
+      if (!partita.inProgramma) {
+        const marcatori = document.createElement("div");
+        marcatori.classList.add("marcatori");
+        marcatori.innerHTML = `
+          <div class="marcatore casa">${partita.marcatoriCasa.join("<br>")}</div>
+          <div class="marcatore ospite">${partita.marcatoriOspite.join("<br>")}</div>
+        `;
+        overlay.appendChild(marcatori);
+
+        const divise = document.createElement("div");
+        divise.classList.add("divise");
+        divise.innerHTML = `
+          <img class="divisa" src="${partita.magliaCasa}">
+          <img class="pallone" src="${partita.pallone}">
+          <img class="divisa" src="${partita.magliaOspite}">
+        `;
+        overlay.appendChild(divise);
+      }
+
+      const note = document.createElement("div");
+      note.classList.add("note");
+      note.textContent = partita.note;
+      overlay.appendChild(note);
+
+      dettagli.appendChild(overlay);
+      dettagli.style.display = "none";
+
+      // toggle singola partita
+      partitaEl.addEventListener("click", (e) => {
+        e.stopPropagation(); // evita propagazione al click della giornata
+        const isOpen = partitaEl.classList.toggle("aperta");
+        dettagli.style.display = isOpen ? "block" : "none";
+      });
+
+      partitaEl.appendChild(titolo);
+      partitaEl.appendChild(dettagli);
+      contenuto.appendChild(partitaEl);
+    });
+
+    // toggle giornata
+    anteprima.addEventListener("click", () => {
+      const isOpen = giornataEl.classList.toggle("aperta");
+
+      if (isOpen) {
+        contenuto.style.display = "block";
+      } else {
+        contenuto.style.display = "none";
+        // chiudi tutte le partite
+        contenuto.querySelectorAll(".partita").forEach(p => {
+          p.classList.remove("aperta");
+          p.querySelector(".dettagli").style.display = "none";
+        });
+      }
+    });
+
+    giornataEl.appendChild(anteprima);
+    giornataEl.appendChild(contenuto);
+    calendario.appendChild(giornataEl);
   });
 }
 
-function toggleDetails(element) {
-  element.classList.toggle("aperta");
-}
-
-document.addEventListener('DOMContentLoaded', generaPartite);
+document.addEventListener("DOMContentLoaded", generaCalendario);
